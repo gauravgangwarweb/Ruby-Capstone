57254ea5d5ac8ef65a13c7aa70f9a59172112813
PrantoshB <pbiswas1007@gmail.com>
2023-03-22 13:47:31 +0530

    Add options to musicalbum class

--- musicalbum.rb
+++ musicalbum.rb
@@ -19,3 +19,28 @@ class MusicAlbum < Item
   attr_accessor :on_spotify, :publish_date
   attr_reader :id
 end
+
+def create_music
+  puts 'Is it on spotify? [y/n]: '
+  on_spotify = gets.chomp
+  puts 'Please state when was the music published [yyyy/mm/dd]:'
+  publish_date = gets.chomp
+  print 'Music Album Added Succesfully'
+  new_music = MusicAlbum.new(on_spotify, publish_date)
+  @music_list.push(new_music)
+end
+
+def add_music(on_spotify, publish_date)
+  new_music = MusicAlbum.new(on_spotify, publish_date)
+  @music_list << new_music
+end
+
+def list_music
+  if @music_list.empty?
+    puts 'Music list is empty'
+  else
+    @music_list.each_with_index do |music, index|
+      puts "(#{index}) ID: #{music.id} On Spotify: \"#{music.on_spotify}\", Publish Date: #{music.publish_date} Archived: #{music.archived}"
+    end
+  end
+end
