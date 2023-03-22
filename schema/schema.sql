8391d0679dcafd579f6a6b3d9d0f4bba8b29f260
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-22 17:41:03 +0530

    Add schema for game and author

--- schema/schema.sql
+++ schema/schema.sql
@@ -24,3 +24,39 @@ CREATE TABLE genre (
   item_id int FOREIGN KEY REFERENCES item(id),
   name varchar
 );
+
+-- Game class table
+CREATE TABLE game (
+  id SERIAL PRIMARY KEY ,
+  item_id INT FOREIGN KEY REFERENCES item(id),
+  multiplayer VARCHAR,
+  last_played_at DATE,
+  publish_date DATE,
+);
+
+-- Author class table
+CREATE TABLE author (
+  id SERIAL PRIMARY KEY,
+  item_id INT FOREIGN KEY REFERENCES item(id),
+  first_name VARCHAR,
+  last_name VARCHAR
+);
+
+-- Create Books table
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  publisher VARCHAR,
+  cover_state VARCHAR,
+  genre_id INT NULL REFERENCES genres(id),
+  author_id INT NULL REFERENCES authors(id),
+  label_id INT NULL REFERENCES labels(id),
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL
+);
+
+-- Create Labels tables
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(150) NOT NULL,
+  color VARCHAR(150) NOT NULL
+);
