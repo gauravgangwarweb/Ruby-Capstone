59677065d7a8a0eac5a8520b854160f7a5e703f8
PrantoshB <pbiswas1007@gmail.com>
2023-03-22 13:25:16 +0530

    Create method to store music albums and genre in JSON file

--- json.rb
+++ json.rb
@@ -8,4 +8,6 @@ def create_files
   Dir.mkdir('./storage')
   FileUtils.touch('./storage/book.json')
   FileUtils.touch('./storage/label.json')
+  FileUtils.touch('./storage/music_albums.json')
+  FileUtils.touch('./storage/genre.json')
 end
